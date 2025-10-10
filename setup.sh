d79e6618e54eb06919405cb7cce4be26f7cd10cb
Daan v O <daan.vo@hotmail.com>
2025-10-10 15:30:41 +0100

    Attempt to make disk permission r/w to others

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo apt install exfat-fuse exfatprogs jellyfin qbittorrent-nox -y
 sudo systemctl enable jellyfin
 sudo systemctl start jellyfin
 sudo mkdir -p /mnt/kingston
-echo "UUID=DEB5-DB3D   /mnt/kingston   exfat   defaults,nofail,uid=1000,gid=1000   0   0" >> /etc/fstab
+echo "UUID=DEB5-DB3D   /mnt/kingston   exfat   defaults,nofail,uid=1000,gid=1000,umask=000   0   0" >> /etc/fstab
 sudo ln -sf "$(pwd)/qbittorrent.service" /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable qbittorrent
